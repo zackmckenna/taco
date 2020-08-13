2a44b9919780dcac3a7852596db7de4c3eee729e
Zack McKenna <zackmckenna@gmail.com>
2020-08-13 12:57:53 -0400

    added geolocation

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,14 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Link } from '@redwoodjs/router'
 import CandidateCell from 'src/components/CandidateCell'
+import { Button } from 'react-bootstrap'
 
 const HomePage = () => {
   const [searchTerm, setSearchTerm] = useState('taco')
+  const [findCandidate, setFindCandidate] = useState(false)
+  const [latitude, setLatitude] = useState(null)
+  const [longoitude, setLongitude] = useState(null)
+  useEffect(() => {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(function (position) {})
+    } else {
+      console.log('location not available')
+    }
+  })
+  const handleFindCandidate = () => {
+    setFindCandidate(!findCandidate)
+  }
+
   return (
     <>
       <h1>Taco</h1>
       <p>Find tacos near you</p>
-      <CandidateCell searchTerm={searchTerm} />
+      <Button onClick={() => handleFindCandidate()}>Taco</Button>
+      {findCandidate && <CandidateCell searchTerm={searchTerm} />}
     </>
   )
 }
