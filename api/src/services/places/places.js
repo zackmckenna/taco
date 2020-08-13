9070fb8eb8a1bbe15dce3fa6fc9964ee901c10cd
Zack McKenna <zackmckenna@gmail.com>
2020-08-13 12:24:18 -0400

    added places service

--- api/src/services/places/places.js
+++ api/src/services/places/places.js
@@ -1,43 +1,15 @@
-export const getPlaces = ({ taco }) => {
+import fetch from 'node-fetch'
+require('dotenv').config()
+
+export const getPlaces = async ({ searchTerm }) => {
+  const response = await fetch(
+    `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${searchTerm}&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=${process.env.PLACES_API_KEY}`
+  )
+
+  const json = await response.json()
+
   return {
-    candidates: [
-      {
-        formatted_address:
-          '2401 E Norris St, Philadelphia, PA 19125, United States',
-        geometry: {
-          location: {
-            lat: 39.97683480000001,
-            lng: -75.1272262,
-          },
-          viewport: {
-            northeast: {
-              lat: 39.97818292989272,
-              lng: -75.12592532010729,
-            },
-            southwest: {
-              lat: 39.97548327010728,
-              lng: -75.12862497989273,
-            },
-          },
-        },
-        name: 'Loco Pez',
-        opening_hours: {
-          open_now: false,
-        },
-        photos: [
-          {
-            height: 3024,
-            html_attributions: [
-              'https://maps.google.com/maps/contrib/110764036160326959908',
-            ],
-            photo_reference:
-              'CmRaAAAAHc2c8UaQYs86l14I8TmrO-djDsEQETDcoujUguiqlzvM4rMncnGC3xTHPRhwxsk1_kKOfr5EcGFDg4Ve-UOXUjbOvuLlVmo76AeEd_zWGjQoDGQkJWMAESR7lsEGpI81EhCDBvFXw512UEyWsAxGBimeGhSPrygkXBIU1kU4ff_kmAmhVKXlwA',
-            width: 4032,
-          },
-        ],
-        rating: 4.5,
-      },
-    ],
-    status: 'OK',
+    searchTerm,
+    candidates: json.candidates,
   }
 }
