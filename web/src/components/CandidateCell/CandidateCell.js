3eb1e6ce9566ca80b6b4c61f1531a287ad1c7b19
Zack McKenna <zackmckenna@gmail.com>
2020-08-13 21:00:20 -0400

    added error handling for no more tacos

--- web/src/components/CandidateCell/CandidateCell.js
+++ web/src/components/CandidateCell/CandidateCell.js
@@ -54,42 +54,53 @@ export const Failure = ({ error }) => <div>Error: {error.message}</div>
 
 export const Success = ({ candidate }) => {
   const [currentCandidate, setCurrentCandidate] = useState(0)
-  const address = candidate.candidates[currentCandidate].vicinity
-    .split(' ')
-    .join('+')
+  if (candidate.candidates[currentCandidate]) {
+    const address = candidate.candidates[currentCandidate].vicinity
+      .split(' ')
+      .join('+')
 
-  console.log(candidate.candidates[currentCandidate])
-  return (
-    <>
-      <Row>
-        <Col>
-          <Card className="mx-auto rounded" style={{ width: '18rem' }}>
-            <Card.Img
-              variant="top"
-              src={candidate.candidates[currentCandidate].photos[0].photo_url}
-            />
-            <Card.Body>
-              <Card.Title>
-                {candidate.candidates[currentCandidate].name}
-              </Card.Title>
-              <Card.Text>
-                {candidate.candidates[currentCandidate].vicinity}
-              </Card.Text>
-              <a
-                className="stretched-link"
-                href={`https://www.google.com/maps/place/${address}`}
-              ></a>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
+    console.log(candidate.candidates[currentCandidate])
+    return (
+      <>
+        <Row>
+          <Col>
+            <Card className="mx-auto rounded" style={{ width: '18rem' }}>
+              <Card.Img
+                variant="top"
+                src={candidate.candidates[currentCandidate].photos[0].photo_url}
+              />
+              <Card.Body>
+                <Card.Title>
+                  {candidate.candidates[currentCandidate].name}
+                </Card.Title>
+                <Card.Text>
+                  {candidate.candidates[currentCandidate].vicinity}
+                </Card.Text>
+                <a
+                  className="stretched-link"
+                  href={`https://www.google.com/maps/place/${address}`}
+                ></a>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="text-center">
+            <button onClick={() => setCurrentCandidate(currentCandidate + 1)}>
+              Taco Me Again
+            </button>
+          </Col>
+        </Row>
+      </>
+    )
+  } else {
+    return (
       <Row>
         <Col className="text-center">
-          <button onClick={() => setCurrentCandidate(currentCandidate + 1)}>
-            Taco Me Again
-          </button>
+          <h1 className="bg-warning p-3">No more tacos here!</h1>
+          <p>more food types coming soon...</p>
         </Col>
       </Row>
-    </>
-  )
+    )
+  }
 }
